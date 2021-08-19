d5b93de2d1ae3e5e3558c6ed46362261210fc168
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-19 12:20:10 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,18 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
 import math
-
 print("give me x1:")
 x1=float(input())
-
 print("give me x2:")
 x2=float(input())
-
 print("give me y1:")
 y1=float(input())
-
 print("give me y2:")
 y2=float(input())
-
 m=(y2-y1)/(x2-x1)
 print("pendiente=", m)
