1f6c60d6581a59905f6fb5e96272c79171a748f4
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-20 11:44:03 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,12 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
     import math
-    print(´Dame el numero de palabras´)
-    words= int(input())
-    pages= math.ceil(words/475)
-    cost= (pages*60)*.90
+    print(`Dame el número de palabras:´)
+    words=int(input())
+    pages=math.ceil(words/475)
+    cost=(pages*60)*.90
     print(`El costo de la publicación es:´, cost)
-    pass
+    pass 
 
 if __name__ == '__main__':
     main()
