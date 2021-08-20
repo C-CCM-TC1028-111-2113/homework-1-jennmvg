a9b2bbe309c4ebcd05874f67cf9c5f8a7c249a28
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-20 10:49:56 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,16 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
     import math
-    print("give me x1:")
+    print("Dame x1:")
     x1=float(input())
-    print("give me x2:")
-    x2=float(input())
-    print("give me y1:")
+    print("Dame x2:")
+    print("Dame y1:")
     y1=float(input())
-    print("give me y2:")
+    x2=float(input())
+    print("Dame y2:")
     y2=float(input())
     m=(y2-y1)/(x2-x1)
-    print("pendiente=", m)
+    print("Pendiente=", m)
     pass
 if __name__ == '__main__':
     main()
