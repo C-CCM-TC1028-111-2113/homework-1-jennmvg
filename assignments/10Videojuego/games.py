25f927d330437ae0bec0cb98ea3e00e0c7e17437
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-19 23:51:46 -0500

    Add files via upload

---
+++ assignments/10Videojuego/games.py
@@ -0,0 +1,7 @@
+import math
+new= int(input("how many new games are you buying?"))
+used= int(input("how many used games are you buying?"))
+newgames= new*1000
+usedgames= used*350
+total= newgames+usedgames
+print("your total is:", total)
