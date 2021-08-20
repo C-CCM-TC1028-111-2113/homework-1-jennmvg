a49cf260e4b92dbdafa595540a2f27690b931cae
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-20 10:53:49 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -3,9 +3,9 @@ def main():
     import math
     print("Dame x1:")
     x1=float(input())
-    print("Dame x2:")
     print("Dame y1:")
-    y1=float(input())
+    y1=float(input()) 
+    print("Dame x2:")
     x2=float(input())
     print("Dame y2:")
     y2=float(input())
