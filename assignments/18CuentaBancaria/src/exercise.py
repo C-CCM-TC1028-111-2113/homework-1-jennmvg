225599b36f676069fc4c19b290d2add59d195574
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-19 23:13:06 -0500

    Update exercise.py

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+    import math
+last= float(input("what was yout last month´s balance?"))
+incomes=float(input("how many incomes did you had?"))
+outcomes= float(input("how many outcomes did you had?"))
+checks= int(input("how many checks did you made?"))
+total= (last*.925)+incomes-outcomes-(checks*13)
+print("your final account balance is:", total)
     pass
 
 if __name__ == '__main__':
