f0103323734fc4cc47b228baa802e672d0e1f403
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-20 11:07:05 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,7 @@
 def main():
     #escribe tu código abajo de esta línea
     import math
-    words= int(input(`Dame el número de palabras:´))
+    words= int(input(`Dame el número de palabras: ´))
     pages= math.ceil(words/475)
     cost= (pages*60)*.90
     print(`El costo de la publicación es:´, cost)
