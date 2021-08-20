143d6a321c4d2cfae4ccb95fc01bc44321fe0a21
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-19 23:46:53 -0500

    Update exercise.py

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
+    import math
+number = int(input("Enter your number: "))
+
+even = 0
+
+while(number > 0):
+  if number % 2 == 0:
+    even += 1
+  number = number // 10
+
+print(f"Number of Even Numbers:", even)
     pass
 
 
