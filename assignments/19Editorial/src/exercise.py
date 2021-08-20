cddff8823405c2156273e070d76dc52ef9cbca6f
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-19 23:23:57 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
+    import math
+words= int(input("how many words did you write?"))
+pages= math.ceil(words/475)
+cost= (pages*60)*.90
+print("Your publiching cost is:", cost)
     pass
 
 
