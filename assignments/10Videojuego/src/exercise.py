6a734c237474d3ececc96e4b216b2716bbab3988
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-19 23:00:15 -0500

    Update exercise.py

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+    import math
+new= int(input("how many new games are you buying?"))
+used= int(input("how many used games are you buying?"))
+newgames= new*1000
+usedgames= used*350
+total= newgames+usedgames
+print("your total is:", total)
    pass
 
 
